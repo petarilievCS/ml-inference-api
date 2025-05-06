e759eea058f8bc46f7cb6c6a5612716afdc88694
Petar Iliev <petariliev@Petars-MacBook-Pro.local>
2025-05-06 08:12:41 -0500

    Classification API

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
 
 app = FastAPI()
+classifier = pipeline("sentiment-analysis")
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, API is working!"}
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/predict")
+async def read_root(input: TextInput):
+    output = classifier(input.text)
+    return {
+        "label": output["label"],
+        "score": round(output["score"], 4)
+    }
